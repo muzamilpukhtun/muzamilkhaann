54b16c7f559818e15eecadbe15db642a72e58cb6
muzamilpukhtun <muzamilpukhtun@gmail.com>
2024-09-16 16:23:04 +0500

    changes

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -4,7 +4,7 @@ import Media from './Media';
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["SysOps Administration.", "DevOps Engineering.", "Site Reliability Engineering.","Infrastructure Modernization Specialist","VPN Administration "],
+      words: ["SysOps Administrator.", "DevOps Engineering.", "Site Reliability Engineering.","Infrastructure Modernization Specialist","VPN Administration "],
       loop: true,
       typeSpeed: 20,
       deleteSpeed: 10,
