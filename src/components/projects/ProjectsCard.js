4944dd2a838f76040fa969278058f2e3660f5ba7
muzamilpukhtun <muzamilpukhtun@gmail.com>
2024-09-16 23:26:29 +0500

    updates

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -4,22 +4,20 @@ import { FaGlobe } from "react-icons/fa";
 
 const ProjectsCard = ({ title, des, src }) => {
   return (
-    <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
-      <div className="w-full h-[80%] overflow-hidden rounded-lg">
+    <div className="w-full p-4 xl:px-6 xl:py-8 h-[400px] rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
+      <div className="w-full h-[60%] overflow-hidden rounded-lg">
         <img
-          className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
+          className="w-full h-full object-cover group-hover:scale-110 duration-300 cursor-pointer"
           src={src}
-          alt="src"
+          alt={title} // Using the title for alt text
         />
       </div>
-      <div className="w-full mt-5 flex flex-col  gap-6">
+      <div className="w-full h-[40%] mt-4 flex flex-col justify-between">
         <div>
-          <div className="flex flex-wrap items-center justify-between">
-            <h3 className="text-base uppercase text-designColor font-normal">
-              {title}
-            </h3>
-          </div>
-          <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+          <h3 className="text-base uppercase text-designColor font-semibold">
+            {title}
+          </h3>
+          <p className="text-sm tracking-wide mt-2 text-gray-300 hover:text-gray-100 duration-300">
             {des}
           </p>
         </div>
@@ -28,4 +26,4 @@ const ProjectsCard = ({ title, des, src }) => {
   );
 }
 
-export default ProjectsCard
+export default ProjectsCard;
